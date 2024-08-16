66caea3eff72503fea5936d2326db54d6a56b9fb
CristianFerreiratx <cristian.ferreira@ucb.edu.bo>
2024-08-16 19:22:04 -0400

    v2

--- Código/PracticasEnClase/PythonIf-Else.py
+++ Código/PracticasEnClase/PythonIf-Else.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     n = int(input().strip())
 if n%2==0 and n<5:
     print("Not Weird")
-elif n>21:
-     print("Not Weird")
+elif n%2==0 and n>21:
+    print("Not Weird")
 else:
-    print("Weird" )
+    print("Weird")
